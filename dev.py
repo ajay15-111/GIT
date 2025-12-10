092f85f209794161d23c340dac983889ac1dfde3
ajay15-111 <ajaypatil1612000@gmail.com>
2025-12-10 08:50:49 +0000

    pycharm

---
+++ dev.py
@@ -0,0 +1,40 @@
+#x=5
+#y=3.14
+#name="Semma"
+
+
+#print(x)
+#print(y)
+#print("your name is "+name)
+
+#name1 =  input("what is your name:")
+#print("Hello, "+ name1)
+
+#num1 = input("Enter the first number:")
+#num2 = input("Enter the second number:")
+#sum = int(num1) + int(num2)
+#print("The sum is " + str(sum))
+
+#num1 = int(input("Enter the first number:"))
+#num2 = int(input("Enter the second number:"))
+#sum = num1 + num2
+#print("The sum is " + str(sum))
+#age =15
+
+#if age>=18:
+#   print("you can vote")
+#else:
+#    print("you cannot vote")
+
+#temp = float(input("Enter the temperature : "))
+
+#if temp > 30 :
+#   print("The temperature is greater than 30")
+#elif temp >= 20 :
+#    print("The temperature is greater than 20")
+#elif temp >= 10 :
+#    print("The temperature is greater than 10")
+#else:
+#    print("The temperature is less than 10")
+
+
